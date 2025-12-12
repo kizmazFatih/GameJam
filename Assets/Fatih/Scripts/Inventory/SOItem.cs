aea4a5a64358adc6b3ed51d62d697c7b227b223e
kizmazFatih <163738158+kizmazFatih@users.noreply.github.com>
2025-12-13 02:43:48 +0300

    Manuel Merge

--- Assets/Fatih/Scripts/Inventory/SOItem.cs
+++ Assets/Fatih/Scripts/Inventory/SOItem.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "ScriptableObjects/Item")]
 public class SOItem : ScriptableObject
 {
-
+    [Header("Tooltip")]
+    public string skillName;
+    [TextArea]
+    public string pickupMessage;
     public PlayerSkill player_skill;
-    //public bool isPlaceable;
+
+    [Header("Görsel")]
     public Texture my_image;
     public GameObject my_prefab;
+
+    [Header("Stok")]
     public int my_amount;
     public int max_stack;
 
